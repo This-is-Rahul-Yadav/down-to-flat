40b1df6123abb17f17315b32bbda9417255a0f21
sansagr <agrawal99sanskar@gmail.com>
2024-08-31 14:13:23 +0530

    [config][setting up mongo config]

--- src/main/java/com/mate_matcher/storage/MongoStorage.java
+++ src/main/java/com/mate_matcher/storage/MongoStorage.java
@@ -17,35 +17,50 @@ import org.bson.types.ObjectId;
 import javax.print.Doc;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
+import java.io.InputStream;
 
 public class MongoStorage {
     public MongoClient mongoClient;
-        public MongoClient connect(){
-            String uri = "mongodb+srv://agrawal99sanskar:<>@cluster0.9myihrx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
-            ServerApi serverApi = ServerApi.builder()
-                    .version(ServerApiVersion.V1)
-                    .build();
-
-            MongoClientSettings settings = MongoClientSettings.builder()
-                    .applyConnectionString(new ConnectionString(uri))
-                    .serverApi(serverApi)
-                    .build();
-
-            mongoClient = MongoClients.create(settings);
-
-
-                MongoDatabase database = mongoClient.getDatabase("sample_mflix");
-                try {
-                    // Send a ping to confirm a successful connection
-                    Bson command = new BsonDocument("ping", new BsonInt64(1));
-                    Document commandResult = database.runCommand(command);
-                    System.out.println("Pinged your deployment. You successfully connected to MongoDB!");
-                } catch (MongoException me) {
-                    System.err.println(me);
-                }
-            return mongoClient;
+    public MongoClient connect(){
+        Properties prop = new Properties();
+        try (InputStream input = MongoStorage.class.getClassLoader().getResourceAsStream("com/config/config.properties")) {
+            if (input == null) {
+                System.out.println("Sorry, unable to find config.properties");
+                return null;
             }
+            prop.load(input);
+        }
+        catch (Exception e){
+            System.out.println(e);
+        }
+
+
+        String uri = prop.getProperty("database.url");
+
+        ServerApi serverApi = ServerApi.builder()
+            .version(ServerApiVersion.V1)
+            .build();
+
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .applyConnectionString(new ConnectionString(uri))
+                .serverApi(serverApi)
+                .build();
+
+        mongoClient = MongoClients.create(settings);
+
+
+        MongoDatabase database = mongoClient.getDatabase("sample_mflix");
+        try {
+            // Send a ping to confirm a successful connection
+            Bson command = new BsonDocument("ping", new BsonInt64(1));
+            Document commandResult = database.runCommand(command);
+            System.out.println("Pinged your deployment. You successfully connected to MongoDB!");
+        } catch (MongoException me) {
+            System.err.println(me);
+        }
+        return mongoClient;
+    }
 
     public void closeConnection(MongoClient mongoClient) {
         if (mongoClient != null) {
